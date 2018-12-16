82a65073007424a20862a9af957b3c96957534b7
Franklin Sarmiento <franklinitiel@gmail.com>
2018-12-16 16:59:58 -0500

    setup.py file created to define the package to deploy to pypi.org.

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="django_semantic_ui",
+    version="1.0.0",
+    author="Franklin Sarmiento",
+    author_email="franklinitiel@gmail.com",
+    description="Library to easy install, configure and use Semantic UI framework with Django project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/franklintiel/django-semantic-ui/wiki",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords="semantic-ui semanticui semantic ui, django-semantic-ui django-semanticui django-semantic-ui",
+    project_urls={
+        'Documentation': "https://github.com/franklintiel/django-semantic-ui",
+        'Source': "https://github.com/franklintiel/django-semantic-ui",
+        'Tracker': "https://github.com/franklintiel/django-semantic-ui/issues"
+    },
+    packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests*']),
+    python_requires=">=2.*")
